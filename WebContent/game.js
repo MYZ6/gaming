89d6668110d3bf5de834238d79393af9288a09c0
lyz19890927 <lyz19890927@163.com>
2016-09-12 12:59:21 +0800

    init

---
+++ WebContent/game.js
@@ -0,0 +1,19 @@
+﻿$(function() {
+	refreshPool();
+});
+
+function refreshPool() {
+	$.ajax({
+		type : "GET",
+		url : 'data/pool.json',
+		error : function() {
+			console.error("query failed");
+		},
+		success : function(data) {
+			console.log(data);
+			$('#dragon-amount v').html(data.dragon);
+			$('#tie-amount v').html(data.tie);
+			$('#tiger-amount v').html(data.tiger);
+		}
+	});
+}
