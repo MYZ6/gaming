1397aa3ad47babf44bbf56c6dec0f56923f765a4
lyz19890927 <lyz19890927@163.com>
2016-09-12 15:44:02 +0800

    完成初步布局

--- WebContent/game.js
+++ WebContent/game.js
@@ -1,4 +1,5 @@
 ﻿$(function() {
+	initData();
 	refreshPool();
 });
 
@@ -14,6 +15,23 @@ function refreshPool() {
 			$('#dragon-amount v').html(data.dragon);
 			$('#tie-amount v').html(data.tie);
 			$('#tiger-amount v').html(data.tiger);
+		}
+	});
+}
+
+function initData() {
+	$.ajax({
+		type : "GET",
+		url : 'data/table.json',
+		error : function() {
+			console.error("query failed");
+		},
+		success : function(data) {
+			$('#table-no v').html(data.tableNo);
+			$('#play-no v').html(data.playNo);
+			$('#seq-no v').html(data.seqNo);
+			$('#up-limit v').html(data.upLimit);
+			$('#down-limit v').html(data.downLimit);
 		}
 	});
 }
